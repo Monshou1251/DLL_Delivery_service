cd7962c20f0e5f8f1f7ca0605b5003334b6fba9f
Monshoux <grigory.urchenko@gmail.com>
2024-09-13 13:50:42 +0700

    before deploy

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Dnipro',
-  plugins: [
-    vue(),
-  ],
+  base: '/Dnipro_LLC',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
